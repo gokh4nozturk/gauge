68d929599b752560091f56c260c04896730965de
gokhanozturk <gokhaanozturk@gmail.com>
2025-05-19 18:39:31 +0300

    Add Header component and view transition animations
    - Introduced a new Header.vue component featuring a title, description, version, license, and credits for the Gauge project.
    - Implemented buttons for GitHub navigation and a playground link.
    - Added CSS animations for view transitions to enhance visual effects during component changes.

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import ModeToggle from '@/components/ModeToggle.vue'
+import { Button } from '@/components/ui/button'
+
+function openGitHub() {
+  window.open('https://github.com/gokh4nozturk/gauge', '_blank')
+}
+</script>
+<template>
+  <header
+    class="p-4 h-52 container mx-auto bg-gradient-to-b rounded-lg from-[#f9f9f9] to-[#e2e8f0] dark:from-[#1c1917] dark:to-[#111827]"
+  >
+    <div class="flex justify-between h-full">
+      <div class="grid grid-rows-[1fr_auto] gap-4">
+        <div class="flex flex-col gap-2">
+          <h1 class="text-2xl font-bold">Gauge</h1>
+          <p class="text-muted-foreground">
+            A simple and customizable circular gauge component for Vue 3.
+          </p>
+        </div>
+        <div class="flex flex-col gap-2">
+          <p class="text-sm text-muted-foreground"><span class="font-bold">Version:</span> 0.0.1</p>
+          <p class="text-sm text-muted-foreground"><span class="font-bold">License:</span> MIT</p>
+          <p class="text-sm text-muted-foreground">
+            <span class="font-bold">Credits:</span>
+            <a href="https://gauge.onur.dev" target="_blank"> Onur Şuyalçınkaya</a>
+          </p>
+        </div>
+      </div>
+      <div class="flex items-end flex-col gap-2">
+        <ModeToggle />
+        <Button variant="link" class="text-xs text-muted-foreground p-0" @click="openGitHub"
+          >View on GitHub</Button
+        >
+        <Button variant="link" class="text-xs text-muted-foreground p-0" as-child>
+          <RouterLink to="#playground"> Playground </RouterLink>
+        </Button>
+      </div>
+    </div>
+  </header>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,19 @@ const router = createRouter({
       name: 'home',
       component: () => import('@/views/GaugeDemo.vue'),
     },
+    {
+      path: '/#playground',
+      name: 'playground',
+      component: () => import('@/components/Playground.vue'),
+    },
   ],
+  scrollBehavior(to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else if (to.hash) {
+      return { el: to.hash, behavior: 'smooth' }
+    }
+  },
 })
 
 export default router
