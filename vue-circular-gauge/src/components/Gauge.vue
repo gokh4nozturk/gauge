6bbd8e4287500d77d72f29c2fff08f415f390c64
gokhanozturk <gokhaanozturk@gmail.com>
2025-05-23 16:18:12 +0300

    refactor: enhance idle icon rendering in Gauge component
    - Updated the idle icon's SVG path to improve visual representation.
    - Changed fill to 'none' and adjusted stroke properties for better styling.
    - Modified animation properties for smoother transitions and effects.

--- vue-circular-gauge/src/components/Gauge.vue
+++ vue-circular-gauge/src/components/Gauge.vue
@@ -242,23 +242,31 @@ const iconYOffset = computed(
     <!-- Idle mode icon -->
     <motion.path
       v-if="idleMode"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M5.51324 3.62367L3.76375 8.34731C3.61845 8.7396 3.24433 8.99999 2.826 8.99999H0.75H0V7.49999H0.75H2.47799L4.56666 1.86057C4.88684 0.996097 6.10683 0.988493 6.43776 1.84891L10.5137 12.4463L12.2408 8.1286C12.3926 7.74894 12.7604 7.49999 13.1693 7.49999H15.25H16V8.99999H15.25H13.5078L11.433 14.1868C11.0954 15.031 9.8976 15.023 9.57122 14.1744L5.51324 3.62367Z"
-      :fill="idleIconColor"
-      :transform="`translate(${iconXOffset}, ${iconYOffset}) scale(${iconScale})`"
+      fill="none"
       :stroke="idleIconColor"
+      :stroke-width="strokeWidth / 3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      :transform="`translate(${centerPosition - 12}, ${centerPosition - 12})`"
+      d="M22 12h-2.48a2 2 0 0 0-1.93 1.46l-2.35 8.36a.25.25 0 0 1-.48 0L9.24 2.18a.25.25 0 0 0-.48 0l-2.35 8.36A2 2 0 0 1 4.49 12H2"
+      :initial="{
+        opacity: 1,
+        pathLength: 1,
+        pathOffset: 0,
+      }"
       :animate="{
-        pathLength: [0, 1],
         opacity: [0, 1],
+        pathLength: [0, 1],
+        pathOffset: [1, 0],
       }"
       :transition="{
-        duration: 0.5,
-        ease: 'easeInOut',
+        duration: 0.6,
+        ease: 'linear',
       }"
       :exit="{
-        pathLength: [1, 0],
-        opacity: [1, 0],
+        opacity: 0,
+        pathLength: 1,
+        pathOffset: 0,
       }"
     />
 
